a08e0781fe0ffd83ed7531f7ec8bfe657750255b
ahhzaky <siahmadzaky@gmail.com>
2022-09-07 02:31:00 +0700

    :tada: Convert HTML to Svelte

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://images.unsplash.com/photo-1550565118-3a14e8d0386f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vbmV5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      pledged: 99000,
+      target: 100000,
+      date_end: +new Date("10 September 2022"),
+      profile_photo:
+        "https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg",
+      profile_name: "Pablo Picaso",
+      no_pledges: 0,
+    },
+  ],
+};
