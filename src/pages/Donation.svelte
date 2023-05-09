31889ab04b46fbb1eceb1511db96f280065af1e1
Zaky Lp <siahmadzaky@gmail.com>
2023-05-09 16:00:23 +0700

    error unxpacted token

--- src/pages/Donation.svelte
+++ src/pages/Donation.svelte
@@ -28,16 +28,13 @@
     const newData = await getCharity($params.id);
     newData.pledged = newData.pledged + parseInt(amount);
     try {
-      const res = await fetch(
-        `https://charity-api-bwa.herokuapp.com/charities/${$params.id}`,
-        {
-          method: "PUT",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(newData),
-        }
-      );
+      const res = await fetch(`http://localhost:8081/charities/${$params.id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(newData),
+      });
       const resMid = await fetch(`/.netlify/functions/payment`, {
         method: "POST",
         headers: {
